d52d870b5591b14f769a825d1c6217a720cb3758
Saraashrf <130941335+Saraashrf@users.noreply.github.com>
2024-04-16 19:25:27 +0200

    Update process.cpp

--- Process/process.cpp
+++ Process/process.cpp
@@ -4,95 +4,100 @@ using namespace std;
 #include "process.h"
 
 
-    process::process(){
-        ID = 0;
-        arrival = 0;
-        burst = 1;
-    }
-    process::process(int id, int arrival_, int burst_){
-        ID = id;
-        arrival = arrival_;
-        burst = burst_;
-    }
+process::process() {
+    ID = 0;
+    arrival = 0;
+    burst = 1;
+}
+process::process(int id, int arrival_, int burst_) {
+    ID = id;
+    arrival = arrival_;
+    burst = burst_;
+}
 
-    int process::getID ()const{
-        return ID;
-    }
-    void process::setID(int id){
-        ID = id;
-    }
+process::process(int id, int arrival_, int burst_, int priority_)
+{
+    ID = id;
+    arrival = arrival_;
+    burst = burst_;
+    priority = priority_;
 
-    int process::getArrival()const{
-        return arrival;
-    }
-    void process::setArrival(int arrival_){
-        arrival = arrival_;
-    }
+}
 
-    int process::getBurst()const{
-        return burst;
-    }
-    void process::setBurst(int burst_){
-        burst = burst_;
-    }
+int process::getID()const {
+    return ID;
+}
+void process::setID(int id) {
+    ID = id;
+}
 
-    int process::getWaitingTime()const{
-        return waitingTime;
-    }
-    void process::setWaitingTime(int waitingTime_){
-        waitingTime = waitingTime_;
-    }
+int process::getArrival()const {
+    return arrival;
+}
+void process::setArrival(int arrival_) {
+    arrival = arrival_;
+}
 
-    int process::getTurnaroundTime()const{
-        return turnaroundTime;
-    }
-    void process::setTurnaroundTime(int turnaroundTime_){
-        turnaroundTime = turnaroundTime_;
-    }
+int process::getBurst()const {
+    return burst;
+}
+void process::setBurst(int burst_) {
+    burst = burst_;
+}
 
-    int process::getDoneBurst()const{
-        return doneBurst;
-    }
-    void process::setDoneBurst(int doneBurst_){
-        doneBurst = doneBurst_;
-    }
+int process::getPriority() const
+{
+    return priority;
+}
 
-    int process::getCurrentTime()const{
-        return currentTime;
-    }
-    void process::setCurrentTime(int currentTime_){
-        currentTime = currentTime_;
-    }
+void process::setPriority(int priority_)
+{
+    priority = priority_;
+}
 
-    //function to execute the process for 1 second
-    void process::execute (){
-        this_thread::sleep_for(chrono::seconds(1));
-        burst--; //NEEDS TO CHANGE`
-    }
-    //function to print remaining time of a process
-    void process::printRemainingTime(){
-        cout << burst - doneBurst << endl;
-    }
+int process::getWaitingTime()const {
+    return waitingTime;
+}
+void process::setWaitingTime(int waitingTime_) {
+    waitingTime = waitingTime_;
+}
 
-    //function to calculate waiting time
-    void process::calcProcessWaitingTime(int finishTime){
-        waitingTime = finishTime - this->burst - this->arrival;
-    }
+int process::getTurnaroundTime()const {
+    return turnaroundTime;
+}
+void process::setTurnaroundTime(int turnaroundTime_) {
+    turnaroundTime = turnaroundTime_;
+}
 
-    //function to calculate turnaround time
-    void process::calcProcessTurnaroundTime(int finishTime){
-        turnaroundTime = finishTime - this->arrival;
-    }
+int process::getDoneBurst()const {
+    return doneBurst;
+}
+void process::setDoneBurst(int doneBurst_) {
+    doneBurst = doneBurst_;
+}
 
-    // custom function to compare processes using arrival time, burst time and current time
-    bool process::compareProcesses(const process& p1) {
+bool process::operator<(const process& other) const {
+    return priority < other.priority;
+}
 
-        if ((arrival <= currentTime) && (p1.arrival <= p1.currentTime))
-            return (burst) < (p1.burst); 
-        else if (arrival <= currentTime)
-            return true;    
-        else
-            return false;
-    }
+//function to execute the process for 1 second
+void process::execute() {
+    this_thread::sleep_for(chrono::seconds(1));
+    burst--; //NEEDS TO CHANGE`
+}
+//function to print remaining time of a process
+void process::printRemainingTime() {
+    cout << burst << endl;
+}
+
+//function to calculate waiting time
+void process::calcProcessWaitingTime(int finishTime) {
+    waitingTime = finishTime - this->burst - this->arrival;
+}
+
+//function to calculate turnaround time
+void process::calcProcessTurnaroundTime(int finishTime) {
+    turnaroundTime = finishTime - this->arrival;
+}
 
 
