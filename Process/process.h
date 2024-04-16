d5e1b02527206f51cb0f90a6b5b5011350bd1555
Saraashrf <130941335+Saraashrf@users.noreply.github.com>
2024-04-16 19:24:19 +0200

    Update process.h

--- Process/process.h
+++ Process/process.h
@@ -10,33 +10,34 @@ private:
     int ID;
     int arrival;
     int burst;
-    int doneBurst=0;
-    int waitingTime=0;
-    int turnaroundTime=0;
-    int currentTime = 0;
+    int doneBurst = 0;
+    int waitingTime = 0;
+    int turnaroundTime = 0;
+    int priority = 3;
 
 public:
     process();
     process(int id, int arrival_, int burst_);
+    process(int id, int arrival_, int burst_, int priority_);
     int getID() const;
     void setID(int id);
     int getArrival() const;
     void setArrival(int arrival_);
     int getBurst() const;
     void setBurst(int burst_);
+    int getPriority()const;
+    void setPriority(int priority_);
+    bool operator<(const process& other) const;
     int getWaitingTime() const;
     void setWaitingTime(int waitingTime_);
     int getTurnaroundTime() const;
     void setTurnaroundTime(int turnaroundTime_);
     int getDoneBurst() const;
-    void setDoneBurst(int currentTime_);
-    int getCurrentTime() const;
-    void setCurrentTime(int currentTime_);
+    void setDoneBurst(int doneBurst_);
     void execute();
     void printRemainingTime();
     void calcProcessWaitingTime(int finishTime);
     void calcProcessTurnaroundTime(int finishTime);
-    bool compareProcesses(const process& p1);
 };
 
 #endif
