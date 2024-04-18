4deda716596d806f00356b6a2821c9cc32043bde
MightyMaya <mayasharaf2@gmail.com>
2024-04-18 09:50:20 +0200

    modified sjfnon.cpp

--- GUI/sjfnon.cpp
+++ GUI/sjfnon.cpp
@@ -40,7 +40,7 @@ sjfnon::sjfnon(QWidget *parent)
     connect(timer, &QTimer::timeout, this, &sjfnon::updateAvgTurnt2);
     timer->start(1000); // Start the timer with 1-second interval
     QtConcurrent::run(SJFNon);
-    QtConcurrent::run(add_time);
+
 
 }
 
@@ -67,7 +67,7 @@ void sjfnon::paintEvent(QPaintEvent *event) {
     font.setPointSize(5); // Adjust font size as needed
     painter.setFont(font);
     painter.setBrush(Qt::white);
-    painter.drawRect(10, 290, 780, 120);
+    painter.drawRect(10, 290, 1000, 120);
     for (const auto& p : ganttChart2) {
         QColor color = QColor::fromHsl((p.getID() * 30) % 360, 255, 150); // Adjust saturation and lightness as needed
         painter.setBrush(color);
@@ -81,13 +81,13 @@ void sjfnon::paintEvent(QPaintEvent *event) {
             y = 420;
             x = 30;
             painter.setBrush(Qt::white);
-            painter.drawRect(10, 410, 780, 120);
+            painter.drawRect(10, 410, 1000, 120);
         }
         else if(x>770 && y == 420){
             y = 300;
             x = 30;
             painter.setBrush(Qt::white);
-            painter.drawRect(10, 290, 780, 120);
+            painter.drawRect(10, 290, 1000, 120);
         }
     }
 }
@@ -100,7 +100,8 @@ void sjfnon::updateChart() {
 
 void SJFNon() {
     isRunning2 = true; // Set the flag to indicate RoundRobin is running
-    std::this_thread::sleep_for(std::chrono::milliseconds(1000)); // Sleep for 1 second
+    QtConcurrent::run(add_time);
+    //std::this_thread::sleep_for(std::chrono::milliseconds(1000)); // Sleep for 1 second
     while (isRunning2) { // Loop as long as the flag is true
         int currentTime = 0;
         while (!readyQueue2.empty()) {
